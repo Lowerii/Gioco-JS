5ff46bd5a0b44d1a6a91386f2b97ba41e42dde7c
Loweri <83225654+Lowerii@users.noreply.github.com>
2024-04-03 11:27:27 +0200

    Project added

---
+++ script/logic/game.js
@@ -0,0 +1,119 @@
+import Player from "../models/player.js";
+import Sound from "../models/sound.js";
+import Sprite from "../models/sprite.js";
+import Hitbox from "../models/hitbox.js";
+
+class Game {
+    playerNickname;
+
+    constructor(canvas, config, playerNickname) {
+        this.config = config
+        this.canvas = canvas;
+        this.playerNickname = playerNickname;
+        this.ctx = canvas.getContext('2d');
+        this.bullets = []
+    }
+
+    init() {
+        this.canvas.style.position = 'absolute';
+        this.canvas.width = this.config.BG_WIDTH;
+        this.canvas.height = this.config.BG_HEIGHT;
+        this.canvas.style.backgroundImage = "url('" + this.config.BACKGROUND_IMG_SRC + "')";
+        this.canvas.style.backgroundSize = "contain";
+
+        this.ground = new Hitbox(0,40, this.canvas.width, 150);
+        this.player = new Player(this.config.PLAYER_SRC, this.playerNickname, 50);
+        this.gigi = new Player(this.config.PLAYER_SRC, "Gigi", 700);
+        this.obstacle = new Hitbox(550, 200, 100, 200);
+        this.bgMusic = new Sound("assets/audio/background.mp3");
+    }
+
+    keyboardPressedHandler(key) {
+        switch(key) {
+            case "d":
+                this.player.velocity.x = this.config.WALK_SPEED;
+                break;
+            case "a":
+                this.player.velocity.x = -this.config.WALK_SPEED;
+                break;
+            case " ":
+                this.player.jump(); 
+                break;
+            case "w":
+                this.player.addSpeed(0, 3);
+                break;
+            case "g":
+                this.bullets.push(this.player.shoot(this.ctx));
+                break;
+        }
+    }
+
+    keyboardReleasedHandler(key) {
+        switch(key) {
+            case "d":
+            case "a":
+                this.player.velocity.x = 0;
+                break;
+            case "w":
+                this.player.setSpeed(0, 0);
+                break;
+        }
+    }
+
+    update() {
+        if(this.player.collision(this.ground)){
+            if(this.player.velocity.y < 0){
+                this.player.velocity.y = 0;
+                this.player.canJump = true;
+                this.player.position.y = this.ground.position.y + this.player.height;
+                // console.log("Player pos: ", this.player.position);
+                // console.log("Ground: ", this.ground.position);
+            }
+            
+        }
+
+
+        if(this.gigi.collision(this.ground)){
+            if(this.gigi.velocity.y < 0){
+                this.gigi.velocity.y = 0;
+                this.gigi.canJump = true;
+                this.gigi.position.y = this.ground.position.y + this.gigi.height;
+                // console.log("Player pos: ", this.player.position);
+                // console.log("Ground: ", this.ground.position);
+            }
+            
+        }
+
+        
+        this.bullets.forEach((b, i) => {
+            b.update()
+            if(this.gigi.health > 0 && b.collision(this.gigi)) {
+                this.gigi.removeHealth(10);
+                this.bullets.splice(i, 1)
+            }
+        })
+        this.player.update();
+        this.gigi.update();
+    }
+
+    playBgMusic() {
+        this.bgMusic.play();
+    }
+
+    stopBgMusic() {
+        this.bgMusic.stop();
+    }
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.bullets.forEach((b) => b.draw(this.ctx))
+        this.player.draw(this.ctx);
+        this.gigi.draw(this.ctx);
+        this.ground.draw(this.ctx);
+        this.obstacle.draw(this.ctx);
+        
+    }
+
+}
+
+export default Game;
